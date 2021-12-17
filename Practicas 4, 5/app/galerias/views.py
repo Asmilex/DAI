0f9919509df5f00ace33ff96f94d63957cf29109
Asmilex <andresmm@outlook.com>
2021-12-17 10:50:04 +0100

    Permisos en views

--- Practicas 4, 5/app/galerias/views.py
+++ Practicas 4, 5/app/galerias/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib import messages
 
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.models import Permission
+
 
 from .forms import *
 from .models import Cuadro, Galeria
@@ -24,7 +27,12 @@ def mostrar_galerias(request):
 # ────────────────────────────────────────────────────────────────────────────────
 
 
+@permission_required('galerias.add_cuadro')
 def crear_cuadro(request):
+    model_name = 'cuadro'
+    all_perms_on_this_modal = Permission.objects.filter(codename__contains=model_name)
+    print(all_perms_on_this_modal)
+
     if 'nombre' in request.POST:
         form = CrearCuadroForm(request.POST, request.FILES)
 
@@ -42,6 +50,7 @@ def crear_cuadro(request):
     return render(request, 'index.html', {'crear_cuadro_form': form})
 
 
+@permission_required('galerias.add_galeria')
 def crear_galeria(request):
     if 'nombre' in request.POST:
         form = CrearGaleriaForm(request.POST)
@@ -63,28 +72,30 @@ def crear_galeria(request):
 
 # ────────────────────────────────────────────────────────────────────────────────
 
-
+@permission_required('galerias.delete_galeria')
 def borrar_galeria(request):
     form = SeleccionarGaleria(request.POST)
 
     if form.is_valid():
         Galeria.objects.filter(id = form['galerias'].value()).delete()
 
-    return render(request, 'index.html', {'seleccionar_galeria_form': SeleccionarGaleria()})
+    return render(request, 'index.html', {'borrar_galeria_form': SeleccionarGaleria()})
 
 
+@permission_required('galerias.delete_cuadro')
 def borrar_cuadro(request):
     form = SeleccionarCuadro(request.POST)
 
     if form.is_valid():
         Cuadro.objects.filter(id = form['cuadros'].value()).delete()
 
-    return render(request, 'index.html', {'seleccionar_cuadro_form': SeleccionarCuadro()})
+    return render(request, 'index.html', {'borrar_cuadro_form': SeleccionarCuadro()})
 
 
 # ────────────────────────────────────────────────────────────────────────────────
 
 
+@permission_required('galerias.change_galeria')
 def actualizar_galeria(request, pk):
     if pk:
         instancia = Galeria.objects.get(pk = pk)
@@ -99,6 +110,7 @@ def actualizar_galeria(request, pk):
     return render(request, 'index.html')
 
 
+@permission_required('galerias.change_cuadro')
 def actualizar_cuadro(request, pk):
     if pk:
         instancia = Cuadro.objects.get(pk = pk)
@@ -113,6 +125,7 @@ def actualizar_cuadro(request, pk):
     return render(request, 'index.html')
 
 
+@permission_required('galerias.change_galeria')
 def formulario_edit_galeria(request):
     form = SeleccionarGaleria(request.POST)
 
@@ -121,13 +134,14 @@ def formulario_edit_galeria(request):
     if form.is_valid():
         instancia = Galeria.objects.get(id = form['galerias'].value())
         context['actualizar_galeria_form'] = CrearGaleriaForm(instance = instancia)
-        context['id_galeria'] = instancia.id
+        context['id_edit_galeria'] = instancia.id
     else:
         context['seleccionar_galeria_form'] = SeleccionarGaleria()
 
     return render(request, 'index.html', context)
 
 
+@permission_required('galerias.change_cuadro')
 def formulario_edit_cuadro(request):
     form = SeleccionarCuadro(request.POST)
 
@@ -136,7 +150,7 @@ def formulario_edit_cuadro(request):
     if form.is_valid():
         instancia = Cuadro.objects.get(id = form['cuadros'].value())
         context['actualizar_cuadro_form'] = CrearCuadroForm(instance = instancia)
-        context['id_cuadro'] = instancia.id
+        context['id_edit_cuadro'] = instancia.id
     else:
         context['seleccionar_cuadro_form'] = SeleccionarCuadro()
 
